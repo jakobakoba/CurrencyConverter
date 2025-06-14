a3575c4805224fba10ede0424b889ff1a1a1278d
jakobakoba <borisgabyshev96dev@gmail.com>
2025-06-14 12:09:57 +0900

    create screens

---
+++ presentation/src/main/java/com/bor96dev/presentation/screens/TransactionScreen.kt
@@ -0,0 +1,10 @@
+package com.bor96dev.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+
+@Composable
+fun TransactionScreen(){
+
+    Text(text = "Transaction Screen")
+}
